f8be0553f681bfa77df8d276c06af121589ae82c
MCamilaAristizabal <mcamila.aristizabal@chicagobooth.edu>
2021-11-24 18:45:24 +0000

    Half way

---
+++ app/models/movie.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  duration    :integer
+#  image       :string
+#  title       :string
+#  year        :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  director_id :integer
+#
+class Movie < ApplicationRecord
+
+  has_many :bookmarks
+
+  has_many :users, :through => :bookmarks
+end
